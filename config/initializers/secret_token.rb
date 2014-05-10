c8bc7e798cd4d585aa07e70317bfd0cabbb804de
Brittany Ansay <britt.ansay@gmail.com>
2014-05-10 08:54:29 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicropostsScaffolded::Application.config.secret_key_base = '1be1043b10ca678754c928b36f68f93783fc4a1b6a229658218dfcdfc7168e813f15d8d8f50e7aee3f2e3ec778f14d4f5c30f92246c103545d58017a68f6ed90'
